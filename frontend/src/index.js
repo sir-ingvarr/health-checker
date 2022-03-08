8fbe31f5f8d1264d83296aa89c9d178758ba89c6
Igor Berezin <igor.berezin@js-dev.pro>
2022-03-08 20:15:30 +0200

    url ports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,14 @@ import './index.css';
 import App from './App';
 
 async function refresh(callback) {
-    fetch('https://api.is-up.link/get-statuses/')
+    fetch('https://api.is-up.link:24499/get-statuses/')
         .then((result) => {
             result.json().then(callback)
         })
 }
 
 async function connectToWs() {
-    const ws = new WebSocket('ws://ws.is-up.link');
+    const ws = new WebSocket('ws://ws.is-up.link:25945');
     ws.addEventListener('open', function () {
         console.info('ws connected');
     });
